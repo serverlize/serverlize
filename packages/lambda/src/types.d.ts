281c816ae5078a4f51deea22e884f6db8e53c6f9
Hassan Khan <hassan.khan@endemolshine.com>
2018-07-21 18:44:26 +0100

    feat(lambda): Export all handler types

--- packages/lambda/src/types.d.ts
+++ packages/lambda/src/types.d.ts
@@ -1,6 +1,25 @@
 import middy from '@middy/core';
-import { Handler as BaseHandler } from 'aws-lambda';
 
-export type LambdaMiddleware = middy.IMiddyMiddlewareObject;
-export type Handler = BaseHandler;
-export type EnhancedHandler = middy.IMiddy;
+export namespace Serverlize {
+  export namespace Lambda {
+    export type Middleware = middy.IMiddyMiddlewareObject;
+
+    // Re-export `*Handler` types from `aws-lambda`
+    export type Handler = AWSLambda.Handler;
+    export type S3Handler = AWSLambda.S3Handler;
+    export type DynamoDBStreamHandler = AWSLambda.DynamoDBStreamHandler;
+    export type SNSHandler = AWSLambda.SNSHandler;
+    export type CognitoUserPoolTriggerHandler = AWSLambda.CognitoUserPoolTriggerHandler;
+    export type CloudFormationCustomResourceHandler = AWSLambda.CloudFormationCustomResourceHandler;
+    export type CloudWatchLogsHandler = AWSLambda.CloudWatchLogsHandler;
+    export type ScheduledHandler = AWSLambda.ScheduledHandler;
+    export type APIGatewayProxyHandler = AWSLambda.APIGatewayProxyHandler;
+    export type CodePipelineHandler = AWSLambda.CodePipelineHandler;
+    export type CloudFrontRequestHandler = AWSLambda.CloudFrontRequestHandler;
+    export type CloudFrontResponseHandler = AWSLambda.CloudFrontResponseHandler;
+    export type KinesisStreamHandler = AWSLambda.KinesisStreamHandler;
+    export type CustomAuthorizerHandler = AWSLambda.CustomAuthorizerHandler;
+
+    export type EnhancedHandler = middy.IMiddy;
+  }
+}
