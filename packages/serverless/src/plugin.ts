a7adad4f3f6ad71a56dc7bcd2b34906462ee72ea
Hassan Khan <hassan.khan@endemolshine.com>
2018-07-21 18:42:43 +0100

    feat(serverless): Add stub implementation of plugin

--- packages/serverless/src/plugin.ts
+++ packages/serverless/src/plugin.ts
@@ -1,26 +1,49 @@
-import Serverless from 'serverless-types';
+import Serverless from './types/serverless';
 
-export default class ServerlizePlugin {
-  public hooks: {
-    [key: string]: () => Promise<any>;
-  };
+// https://gist.github.com/HyperBrain/50d38027a8f57778d5b0f135d80ea406
 
-  serverless: Serverless;
+export default class ServerlizePlugin extends Serverless.Plugin<Options> {
+  /**
+   * A list of all loaded extensions for the plugin
+   */
+  private static readonly extensions: Serverless.PluginExtension[];
 
   /**
    * Constructor
    */
-  constructor(serverless: Serverless) {
+  constructor(serverless: Serverless, options: Options) {
+    super(serverless, options);
     this.serverless = serverless;
+    this.options = options;
+
+    this.commands = {};
     this.hooks = {
-      'package:compileEvents': this.process,
+      'before:package:compileEvents': this.compileEvents,
     };
   }
+
+  private loadExtensions = () => {
+    ServerlizePlugin.extensions.forEach((extension) => {
+      this.commands[extension.name] = extension.command;
+      this.hooks[extension.name] = extension.hook(this);
+    });
+  };
+
+  private compileEvents = () => {
+    // Read all `function.yml` files from `${APP_PATH}/functions/**/*.yml`
+    return Promise.resolve({ foo: 'bar' });
+  };
+
+  private compileResources = () => {
+    // Check if any models exist under `${APP_PATH}/app/models/`
+    // Check if any topics exist under `${APP_PATH}/app/topics/`
+    return Promise.resolve({ foo: 'bar' });
+  };
 
   /**
    * Process the provided configuration
    */
-  private process = () => {
+  private parseOptions = () => {
     return Promise.resolve({ foo: 'bar' });
   };
 }
