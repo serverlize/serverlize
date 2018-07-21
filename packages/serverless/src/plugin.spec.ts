0295d985640f61c86254faf6f70e331f7bfb9883
Hassan Khan <hassan.khan@endemolshine.com>
2018-07-21 20:14:32 +0100

    build(jest): Share `jest.config.js` across subpackages
    Fixes #12

--- packages/serverless/src/plugin.spec.ts
+++ packages/serverless/src/plugin.spec.ts
@@ -1,6 +1,7 @@
 // import Serverless from 'serverless-types';
 
 import Plugin from './plugin';
+import Serverless from './types/serverless';
 
 describe('Plugin', () => {
   it('creates a response object', async () => {
