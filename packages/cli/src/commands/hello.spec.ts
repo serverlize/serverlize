2ff63ea7ccf6f92641db37d5be84574e03aa8955
Hassan Khan <hassan.khan@endemolshine.com>
2018-08-27 20:28:57 +0100

    chore(cli): upgrade @oclif/config to allow colocated tests

--- packages/cli/src/commands/hello.spec.ts
+++ packages/cli/src/commands/hello.spec.ts
@@ -1,22 +1,22 @@
-import HelloCommand from '../../src/Commands/hello'
+import HelloCommand from './hello';
 
 type ProcessStdOutWrite = (buffer: Buffer | string, cb?: () => void) => boolean;
 
 describe('hello', () => {
-
   let mockStdOut: jest.Mock<ProcessStdOutWrite>;
 
   beforeEach(() => {
-    mockStdOut = jest.spyOn(process.stdout, 'write')
-      .mockImplementation(() => {});
+    mockStdOut = jest.spyOn(process.stdout, 'write').mockImplementation(() => {
+      return;
+    });
   });
 
   it('runs hello', () => {
     HelloCommand.run(['hello'])
       .then(() => {
-        expect(mockStdOut)
-          .toHaveBeenCalledWith(
-            'hello world from ./src/Commands/hello.ts\n');
+        expect(mockStdOut).toHaveBeenCalledWith(
+          'hello world from ./src/Commands/hello.ts\n',
+        );
       })
       // @TODO HORRIBLE HORRIBLE HORRIBLE
       .catch(() => {});
@@ -25,9 +25,9 @@ describe('hello', () => {
   it('runs hello --name jeff', () => {
     HelloCommand.run(['hello', '--name', 'jeff'])
       .then(() => {
-        expect(mockStdOut)
-          .toHaveBeenCalledWith(
-            'hello jeff from ./src/Commands/hello.ts\n');
+        expect(mockStdOut).toHaveBeenCalledWith(
+          'hello jeff from ./src/Commands/hello.ts\n',
+        );
       })
       // @TODO HORRIBLE HORRIBLE HORRIBLE
       .catch(() => {});
